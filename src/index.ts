b24bcd0153de03503d0616867ac2c1a5c8b1d304
khairul anwar <59588004+khairulanwarDeen@users.noreply.github.com>
2021-04-13 11:42:10 +0800

    create migration. and allow push to db
    use mysql siol instead postgresql. noice

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import microConfig from "./mikro-orm.config";
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+
+const main = async () => {
+    
+    const orm = await MikroORM.init(microConfig);
+    console.log("yo wurl");
+    await orm.getMigrator().up();
+
+    const post = orm.em.create(Post, {title: 'my first post'});
+    await orm.em.persistAndFlush(post);
+    //await orm.em.nativeInsert(Post, {title: 'my first post 2'}); //full of errors here
+
+    const posts = await orm.em.find(Post, {});
+    console.log(posts)
+    // const app = express();
+    // const apolloServer = new ApolloServer({
+    //     schema: await buildSchema({}
+    //         resolvers: [],
+    //         validate: false,
+            
+
+    //     })
+    // })
+    // app.get('/', (_, res) => { // to ignore a variable you put the underscore '_'
+        
+    //     res.send("yo whatup");
+    // })
+
+    // app.listen(4000, () => {
+    //     console.log('server start on localhost:4000')
+    // })
+}
+main().catch((err) => {
+    console.error(err);
+}) 
+console.log("yo wurl");
