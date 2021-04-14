581154947df0e91cfc166195ac23405d3616cf8f
khairul anwar <59588004+khairulanwarDeen@users.noreply.github.com>
2021-04-14 14:14:39 +0800

    create migration for new entity, user
    create migration for new entity, user

---
+++ src/entities/user.ts
@@ -0,0 +1,27 @@
+import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, ObjectType } from "type-graphql";
+
+@ObjectType()
+@Entity()
+export class User{
+    @Field()
+    @PrimaryKey()
+    id!: number;
+
+    @Field(() => String)
+    @Property({ type: "date" })
+    createdAt = new Date();
+
+    @Field(() => String)
+    @Property({ type:"date", onUpdate: () => new Date() })
+    updatedAt = new Date();
+
+    @Field() //you can comment this field out and it will not be exposed on the graphql server
+    @Property({type: "text", unique: true})
+    username!: string;
+
+    @Property({type: "text"})
+    password!: string;
+
+}
+
