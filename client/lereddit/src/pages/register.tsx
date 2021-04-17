9bbbe72340ad422f8614086b30680c7fa154bfc6
khairul anwar <59588004+khairulanwarDeen@users.noreply.github.com>
2021-04-17 17:58:40 +0800

    setup graphql mutate request from client to server

--- client/lereddit/src/pages/register.tsx
+++ client/lereddit/src/pages/register.tsx
@@ -3,18 +3,39 @@ import { Formik, Form } from "formik";
 import { FormControl, FormLabel, Input, FormErrorMessage, Box, Button } from '@chakra-ui/react';
 import { Wrapper } from '../components/wrapper';
 import { InputField } from '../components/InputField';
+import { useMutation } from "urql"
 
 interface registerProps {
 
 }
 
+const REGISTER_MUT = `mutation Register($username: String!, $password:String!){
+    register(username: $username, password: $password){
+      errors{
+        field
+        message
+      }
+      user{
+        id
+        username
+        createdAt
+        updatedAt
+      }
+    }
+  }
+  `
+
+
+
 export const Register: React.FC<registerProps> = ({ }) => {
+    const [, register] = useMutation(REGISTER_MUT);
     return (
         <Wrapper variant="regular">
             <Formik
                 initialValues={{ username: "", password: "" }}
                 onSubmit={(values) => {
                     console.log(values);
+                    register({ username: values.username, password: values.password })
                 }}
             >
                 {({ isSubmitting }) => (
