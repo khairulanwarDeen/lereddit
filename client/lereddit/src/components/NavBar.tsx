855b0f9f441d4089ce157447e369bb25f8cc73f9
khairul anwar <59588004+khairulanwarDeen@users.noreply.github.com>
2021-04-20 16:27:27 +0800

    graphcache initialised here
    use cache so that the cookies can update the cache accordingly.

--- client/lereddit/src/components/NavBar.tsx
+++ client/lereddit/src/components/NavBar.tsx
@@ -40,7 +40,7 @@ export const NavBar: React.FC<NavBarProps> = ({ }) => {
         )
     }
     return (
-        <Flex bg="tomato" p={4}>
+        <Flex bg="cadetblue" p={4}>
             <Box p={3} ml={"auto"}>
                 {body}
             </Box>
