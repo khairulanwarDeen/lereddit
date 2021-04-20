0ed32b3c898bedaca3b99bfcff39ec9136e284a3
khairul anwar <59588004+khairulanwarDeen@users.noreply.github.com>
2021-04-20 13:52:16 +0800

    state switch using navbar
    created a me query
    created button on navbar for stateswitching

--- client/lereddit/src/components/NavBar.tsx
+++ client/lereddit/src/components/NavBar.tsx
@@ -1,12 +1,49 @@
-import { Box } from "@chakra-ui/react"
-import React from 'react'
+import { Box, Button, Flex, Link } from "@chakra-ui/react";
+import React from 'react';
+import NextLink from "next/link";
+import { useMeQuery } from "../generated/graphql";
 
 interface NavBarProps {
 
 }
 
 export const NavBar: React.FC<NavBarProps> = ({ }) => {
+    const [{ data, fetching }] = useMeQuery()
+    let body = null;
+
+    //is the page still loading?
+    if (fetching) {
+        body
+
+    }
+    //user not logged in?
+    else if (!data?.me) {
+        body = (
+            <>
+                <NextLink href="/login">
+                    <Link color="white" mr={2}>Login</Link>
+                </NextLink>
+                <NextLink href="/register">
+                    <Link color="white">Register</Link>
+                </NextLink></>
+        )
+    }
+    //user is logged in 
+    else {
+        body = (
+            <Flex>
+                <Box mr={2}>
+                    {data.me.username}
+                </Box>
+                <Button variant="link">Log Out</Button>
+            </Flex>
+        )
+    }
     return (
-        <Box bg="tomato">pretty good</Box>
+        <Flex bg="tomato" p={4}>
+            <Box p={3} ml={"auto"}>
+                {body}
+            </Box>
+        </Flex>
     );
 }
