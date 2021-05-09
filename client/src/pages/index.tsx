cbaa13addd22226239464077478ded58d4980a8d
khairul anwar <59588004+khairulanwarDeen@users.noreply.github.com>
2021-05-09 17:51:57 +0800

    pagination Part1

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,18 +1,34 @@
 import { withUrqlClient } from "next-urql";
-import React from "react";
+import React, { useState } from "react";
 import { Layout } from "../components/layout";
 import { useGetPostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import NextLink from "next/link";
-import { Box, Heading, Link, Text, Stack, Flex } from "@chakra-ui/react";
+import {
+  Box,
+  Heading,
+  Link,
+  Text,
+  Stack,
+  Flex,
+  Button,
+} from "@chakra-ui/react";
 
 const Index = () => {
-  const limit = 15;
-  const [{ data }] = useGetPostsQuery({
-    variables: {
-      limit: limit,
-    },
+  const limitNo = 2;
+  const [variables, setVariables] = useState({
+    limit: limitNo,
+    cursor: null as null | string,
   });
+
+  console.log(variables);
+
+  const [{ data, fetching }] = useGetPostsQuery({
+    variables,
+  });
+  if (!fetching && !data) {
+    return <div>There is no data for some reason</div>;
+  }
   return (
     <Layout>
       <Flex align={"center"}>
@@ -24,20 +40,37 @@ const Index = () => {
         </NextLink>
       </Flex>
 
-      <div>Latest post from the limit of: {limit}</div>
+      <div>Latest post from the limit of: {variables.limit}</div>
       <br />
-      {!data ? (
+      {fetching && !data ? (
         <div>Loading...</div>
       ) : (
         <Stack spacing={8}>
-          {data.getposts.map((p) => (
+          {data!.getposts.map((p) => (
             <Box key={p.id} p={5} shadow="md" borderWidth="1px">
-              <Heading fontSize="large">{p.title}</Heading>
+              <Heading fontSize="large">{p.title + p.id}</Heading>
               <Text mt={4}>{p.textSnippet}</Text>
             </Box>
           ))}
         </Stack>
       )}
+      {data ? (
+        <Flex>
+          <Button
+            onClick={() => {
+              setVariables({
+                limit: variables.limit,
+                cursor: data.getposts[data.getposts.length - 1].createdAt,
+              });
+            }}
+            isLoading={fetching}
+            m="auto"
+            my={8}
+          >
+            Load More
+          </Button>
+        </Flex>
+      ) : null}
     </Layout>
   );
 };
